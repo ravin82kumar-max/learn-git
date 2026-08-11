bd25f18bba60d3c4bbd83221f825f0290778f6c8
ravin82kumar-max <ravin82kumar@gmail.com>
2026-08-11 11:28:00 +0530

    scond change

--- vanakam.js
+++ vanakam.js
@@ -1,2 +1,3 @@
 console.log("Vanakam! Welcome to the Vanakam.js library.");
 console .log("This library provides various utilities for web development.");
+console.log("Feel free to explore the functions and features available in this library.");
